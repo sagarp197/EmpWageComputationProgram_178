f76a5916d37c452570da5d6ea1e9d7558ebabdb5
sagarp197 <sagarp197@gmail.com>
2022-07-31 06:08:45 +0530

    UC1 Code added in Master

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace UC1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Is_Full_Time = 1;
+
+            Random random = new Random();
+            int empCheck = random.Next(2);
+
+            if (empCheck == Is_Full_Time)
+            {
+                Console.WriteLine("Employee is Present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+            }
+        }
+    }
+}
