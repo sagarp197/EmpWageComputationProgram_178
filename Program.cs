ebabc6e337d81e3ee004e1bebb10770f62d7671e
sagarp197 <sagarp197@gmail.com>
2022-08-01 03:50:43 +0530

    added code in UC4

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace UC4
+{
+    class Program
+    {
+        public const int IS_Part_Time = 1;
+        public const int IS_Full_Time = 2;
+        public const int Emp_Rate_Per_Hour = 20;
+        static void Main(string[] args)
+        {
+            int empHrs = 0;
+            int empWage = 0;
+            Random random = new Random();
+            int empCheck = random.Next(0,3);
+
+            switch (empCheck)
+            {
+                case IS_Part_Time:
+                    Console.WriteLine("Employee is Working Part Time");
+                    empHrs = 4;
+                    break;
+
+                case IS_Full_Time:
+                    Console.WriteLine("Employee is Present Full Time");
+                    empHrs = 8;
+                    break;
+
+                default:
+                    Console.WriteLine("Employee is Absent");
+                    empHrs = 0;
+                    break;
+            }
+            empWage = empHrs * Emp_Rate_Per_Hour;
+            Console.WriteLine("Emp Wage :" + empWage);
+        }
+    }
+}
