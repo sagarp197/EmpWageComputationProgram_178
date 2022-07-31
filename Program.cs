a094892fbc9b58ca81eaa74c2ab673d5894f5a39
sagarp197 <sagarp197@gmail.com>
2022-08-01 03:40:32 +0530

    Code added for the UC3

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace UC3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int Emp_Rate_Per_Hour = 20;
+            int empHrs = 0;
+            int empWage = 0;
+
+            Random random = new Random();
+            int empCheck = random.Next(0, 3);
+
+            if (empCheck == 0)
+            {
+                Console.WriteLine("Employee is Present Full Time");
+                empHrs = 8;
+            }
+            else if (empCheck == 1)
+            {
+                Console.WriteLine("Employee is Working Part Time");
+                empHrs = 4;
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+                empHrs = 0;
+            }
+            empWage = empHrs * Emp_Rate_Per_Hour;
+            Console.WriteLine("Emp Wage :" + empWage);
+
+        }
+
+    }
+}
