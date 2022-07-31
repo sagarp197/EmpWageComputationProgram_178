931405d2e3122eaad6b27e5991f441df5b7f13eb
sagarp197 <sagarp197@gmail.com>
2022-07-31 06:33:01 +0530

    Code added for Use case 2

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace UC2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Is_Full_Time = 1;
+            int Emp_Rate_Per_Hour = 20;
+
+            int EmpHrs = 0;
+            int EmpWage = 0;
+
+            Random random = new Random();
+            int empCheck = random.Next(0, 2);
+
+            if (empCheck == Is_Full_Time)
+            {
+                Console.WriteLine("Employee is Working Full Time");
+                EmpHrs = 8;
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+                EmpHrs = 0;
+            }
+
+            EmpWage = EmpHrs * Emp_Rate_Per_Hour;
+            Console.WriteLine("Emp Wage is " + EmpWage);
+        }
+    }
+}
